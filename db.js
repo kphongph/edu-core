9e14be22dcb77383135707019c22c838d84e7ef9
Phongphun Kijsanayothin <kphongph@gmail.com>
2015-09-23 16:35:16 +0700

    small fixed

--- db.js
+++ db.js
@@ -10,12 +10,15 @@ module.exports.query = function(content,cb) {
       cb(err,null);
     }
     var ps = new sql.PreparedStatement(connection);
+    var params = {};
     for(var key in content.params) {
       var type = sql.VarChar;
       ps.input(key,type);
+      params[key]=content.params[key].value;
     }
+    
     ps.prepare(content.query, function(err) {
-      ps.execute(content.params, function(err, recordset) {
+      ps.execute(params, function(err, recordset) {
         if(err) cb(err,null);
         ps.unprepare(function(err) {
           cb(null,recordset);
